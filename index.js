3275c76c40674b9123096270b1f9c9092d77fba3
Oleg Odynets <odynets.oleh@gmail.com>
2018-05-02 12:07:45 +0000

    Initial

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 function solve(a, b, c) {
-var result = (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
-var result2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
-return result + "<br>" + result2;
+var d = b * b - (4 * a * c); 
+if (d > 0) {
+var result1 = (-1 * b + Math.sqrt(d)) / (2 * a);
+var result2 = (-1 * b - Math.sqrt(d)) / (2 * a);
+return result1 + "<br>" + result2;
+    
 }
-document.write(solve(2, 5, 2));
+else if (d === 0) {
+var result3 = (-b) / (2 * a);
+return result3;
+    
+}
+else if (d < 0) {
+return "D < 0, then the quadratic equation has no real solutions(it has 2 complex solutions).";
+    
+}
+
+}
+document.write(solve(1, 3, -4));
 
 
