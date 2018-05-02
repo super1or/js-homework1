6d9e37619fa110969fab4038d2802661cc39743e
Oleg Odynets <odynets.oleh@gmail.com>
2018-05-02 06:56:28 +0000

    Initial

---
+++ index.js
@@ -0,0 +1,8 @@
+function solve(a, b, c) {
+var result = (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+var result2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+return result + "<br>" + result2;
+}
+document.write(solve(2, 5, 2));
+
+
